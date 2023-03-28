c23299b3501ee42217bd589cb2ea5a695cb1ccfd
Chy Meng <chymeng24@gmail.com>
2023-03-28 17:51:51 +1100

    feat: add dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
         pink: "#FFA3AC",
         purple: "#f9e0ff",
+        lightBlack: "#0a0f1b",
       },
     },
     fontFamily: {

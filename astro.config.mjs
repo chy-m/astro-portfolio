e5ade1b49fb1d3d9241addec618bb6a480fe5e95
Chy Meng <chymeng24@gmail.com>
2023-04-01 16:03:30 +1100

    feat: add compress package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    compress(),
   ],
 });
